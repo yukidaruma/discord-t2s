aa3836fd795a158488a061260a22f81fac2d112c
yukidaruma <yukidaruma@users.noreply.github.com>
2020-05-20 22:23:53 +0900

    Reduce audio volume by 50%

--- t2s.js
+++ t2s.js
@@ -10,7 +10,7 @@ const text2speech = async (text) => {
   const [response] = await client.synthesizeSpeech({
     input: { text },
     voice: { languageCode: process.env.T2S_LANGUAGE_CODE },
-    audioConfig: { audioEncoding: 'MP3' },
+    audioConfig: { audioEncoding: 'MP3', volumeGainDb: -3 }, // -3db means 70% of the default volume.
   });
   const { audioContent: audio } = response;
 
