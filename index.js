c28b3442dd0c2a4a535ececf1ee305738e358324
yukidaruma <yukidaruma@users.noreply.github.com>
2020-05-20 14:33:07 +0900

    Add ability to speak text from muted users

--- index.js
+++ index.js
@@ -1,40 +1,91 @@
 const Discord = require('discord.js');
+const { text2speech } = require('./t2s');
 
 const client = new Discord.Client();
 
-/** @type {string} */
-let myUserId;
+/** @type {{
+  myUserId: string;
+  voiceConnection: Discord.VoiceConnection|null;
+}} */
+const state = {
+  myUserId: '',
+  voiceConnection: null,
+};
 
 /** @param {Discord.Message} msg */
 /** @returns {boolean} */
 const isMentioned = (msg) => {
   const mentionedUser = msg.mentions.users.first();
 
-  return mentionedUser && mentionedUser.id === myUserId;
+  return mentionedUser && mentionedUser.id === state.myUserId;
 }
+
+const bangCommand = (command) => `!${command}`;
+
+const internalCommands = {
+  /** @param {Discord.VoiceChannel} voiceChannel */
+  leave(voiceChannel) {
+    voiceChannel.leave();
+    state.voiceConnection = null;
+  }
+};
 
 /** @typedef {(...message: Discord.Message[]) => void} OnMessageCallback */
 /** @type {Object<string, OnMessageCallback>} */
 const commands = {
-  listen: (msg) => {
-    msg.reply('Hey, listen!');
+  async join(msg) {
+    const { channel: voiceChannel } = msg.member.voice;
+
+    if (voiceChannel) {
+      state.voiceConnection = await voiceChannel.join();
+    } else {
+      msg.reply('You have to be in a voice channel.');
+    }
+  },
+  kick(msg) {
+    const { channel: voiceChannel } = msg.guild.me.voice;
+    if (voiceChannel) {
+      internalCommands.leave(voiceChannel);
+    }
   },
 };
+
 const availableCommands = Object.keys(commands);
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
-  myUserId = client.user.id;
+  state.myUserId = client.user.id;
 });
 
-client.on('message', (msg) => {
+client.on('message', async (msg) => {
+  // Ignore own messages
+  if (msg.member.id === state.myUserId) {
+    return;
+  }
+
+  // React to mentions
   if (isMentioned(msg)) {
-    const command = availableCommands.find((command) => msg.cleanContent.includes(`!${command}`));
+    const command = availableCommands.find((command) => msg.cleanContent.includes(bangCommand(command)));
 
     if (command) {
+      console.log(`Running command: ${command}`);
       commands[command](msg);
+      return;
     }
+
+    msg.reply(`No command specified. Available commands: ${availableCommands.map(bangCommand).join(', ')}.`);
+    return;
   }
+
+  // Speak text
+  if (msg.member.voice.mute) {
+    const { cleanContent: text } = msg;
+    console.log(`Speaking: ${text}`);
+    state.voiceConnection.play(await text2speech(text));
+    return;
+  }
+
+  console.log(`User ${msg.member.displayName} is not muted. Skipping.`);
 });
 
 client.login(process.env.DISCORD_CLIENT_SECRET);
---
+++ t2s.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const { Readable } = require('stream');
+const t2s = require('@google-cloud/text-to-speech');
+const client = new t2s.TextToSpeechClient({
+  credentials: JSON.parse(fs.readFileSync('./google-service-account.json')),
+});
+
+/** @returns {Promise<Readable>} audio */
+const text2speech = async (text) => {
+  const [response] = await client.synthesizeSpeech({
+    input: { text },
+    voice: { languageCode: process.env.T2S_LANGUAGE_CODE },
+    audioConfig: { audioEncoding: 'MP3' },
+  });
+  const { audioContent: audio } = response;
+
+  const readable = new Readable();
+  readable._read = () => {};
+  readable.push(audio);
+  readable.push(null);
+  return readable;
+}
+
+module.exports = {
+  text2speech,
+};
